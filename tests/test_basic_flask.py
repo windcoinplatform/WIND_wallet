97df76d61309512c29754be19bdf2104f144d423
bnagtega <bram.nagtegals@trendminer.com>
2019-08-07 09:53:34 +0200

    add basic unit tests

---
+++ tests/test_basic_flask.py
@@ -0,0 +1,50 @@
+import unittest
+from TurtleNetwork import app
+
+
+class BasicTests(unittest.TestCase):
+
+    ########################
+    #### helper methods ####
+    ########################
+
+    def login(self, seed, pk):
+        return self.app.post(
+            '/login',
+            data=dict(seed=seed, pk=pk),
+            follow_redirects=True
+        )
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEqual(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_login_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Log in', response.data)
+
+    def test_login_with_seed(self):
+        response = self.login('a', '',)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Leasing', response.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
